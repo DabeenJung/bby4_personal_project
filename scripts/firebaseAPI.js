6e34404ee03d2ba7d60d47620198461a1ffd6714
Valerie Tan <valerietan000@gmail.com>
2021-11-01 15:35:50 -0700

    added plant info page

--- scripts/firebaseAPI.js
+++ scripts/firebaseAPI.js
@@ -1,12 +1,35 @@
-var firebaseConfig = {
-  apiKey: "AIzaSyCV5dam6cAgP1sWtQpM87MUAvT49dlBjdc",
-  authDomain: "plantapp-72bb6.firebaseapp.com",
-  projectId: "plantapp-72bb6",
-  storageBucket: "plantapp-72bb6.appspot.com",
-  messagingSenderId: "223515710304",
-  appId: "1:223515710304:web:b50f3ce55e67e48cca0405"
-};
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  // Import the functions you need from the SDKs you need
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js";
+
+  // TODO: Add SDKs for Firebase products that you want to use
+
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+  // Your web app's Firebase configuration
+
+  const firebaseConfig = {
+
+    apiKey: "AIzaSyAVO1E3t5LDaEOZBB85V2lahCX5Z2xEMGM",
+
+    authDomain: "plantapp-34d6d.firebaseapp.com",
+
+    projectId: "plantapp-34d6d",
+
+    storageBucket: "plantapp-34d6d.appspot.com",
+
+    messagingSenderId: "1085032504158",
+
+    appId: "1:1085032504158:web:8b0c4fe272a730b8268dcf"
+
+  };
+
+
+  //Initialize Firebase
+
+  const app = initializeApp(firebaseConfig);
+
+
 
