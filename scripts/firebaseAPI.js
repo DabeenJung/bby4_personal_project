412d134276cfb73aba7b9cca7eb11978302ec76d
Valerie Tan <valerietan000@gmail.com>
2021-11-01 13:01:38 -0700

    added firebase authentication and fixed some syntax errors in main.html

---
+++ scripts/firebaseAPI.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCV5dam6cAgP1sWtQpM87MUAvT49dlBjdc",
+  authDomain: "plantapp-72bb6.firebaseapp.com",
+  projectId: "plantapp-72bb6",
+  storageBucket: "plantapp-72bb6.appspot.com",
+  messagingSenderId: "223515710304",
+  appId: "1:223515710304:web:b50f3ce55e67e48cca0405"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
