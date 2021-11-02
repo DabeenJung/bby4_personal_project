662f72eacece9d868a6ff086d05a0c4ae942dab4
DabeenJung <90542859+DabeenJung@users.noreply.github.com>
2021-11-01 18:12:38 -0700

    Updated

--- scripts/firebaseAPI.js
+++ scripts/firebaseAPI.js
@@ -1,35 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC-1-kvwUFSANJ4GSr2I5xNUEKwx31dLS4",
+    authDomain: "bby-04.firebaseapp.com",
+    projectId: "bby-04",
+    storageBucket: "bby-04.appspot.com",
+    messagingSenderId: "88090173696",
+    appId: "1:88090173696:web:5ac0ce05bb89c5f12b93e5"
+};
 
-  // Import the functions you need from the SDKs you need
-
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js";
-
-  // TODO: Add SDKs for Firebase products that you want to use
-
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-
-  // Your web app's Firebase configuration
-
-  const firebaseConfig = {
-
-    apiKey: "AIzaSyAVO1E3t5LDaEOZBB85V2lahCX5Z2xEMGM",
-
-    authDomain: "plantapp-34d6d.firebaseapp.com",
-
-    projectId: "plantapp-34d6d",
-
-    storageBucket: "plantapp-34d6d.appspot.com",
-
-    messagingSenderId: "1085032504158",
-
-    appId: "1:1085032504158:web:8b0c4fe272a730b8268dcf"
-
-  };
-
-
-  //Initialize Firebase
-
-  const app = initializeApp(firebaseConfig);
-
-
-
+const app = firebase.initializeApp(firebaseConfig);
